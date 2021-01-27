ee9d2c6954b1e55e8506cc6d52f25344197249b5
alexjbustillos <alexjbustillos@gmail.com>
2021-01-27 10:05:18 -0500

    final changes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,21 +2,27 @@ import React from 'react';
 
 const Welcome = () => {
   return (
-      <div>
-          <h1>Welcome to Kokoro your personal meditation tracker</h1>
-          <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
-<div className="carousel-inner">
-  <div className="carousel-item active">
-    <img class="wpage" src="/pictures/meditations.png" className="d-block w-100" alt="cannot show"/>
-  </div>
-  <div className="carousel-item">
-    <img class="wpage" src="/pictures/hill.png" className="d-block w-100" alt="cannot show"/>
-  </div>
-  <div className="carousel-item">
-    <img class="wpage" src="/pictures/sun" className="d-block w-100" alt="cannot show"/>
-  </div>
-</div>
-</div>
+      <div className="text-center">
+        <h1>Kokoro</h1>
+        <p>Your Personal Meditation App</p>
+        <div className="card-body">
+                <div className="text-center">
+                    <img  className="profile-img"src="https://i.imgur.com/LPN96ZLl.jpg" alt=""/>
+                </div>
+            </div>
+        {/* <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active">
+              <img class="wpage" src="/pictures/meditations.png" className="d-block w-90" alt="cannot show"/>
+            </div> */}
+            {/* <div className="carousel-item">
+              <img class="wpage" src="/pictures/hill.png" className="d-block w-90" alt="cannot show"/>
+            </div> */}
+            {/* <div className="carousel-item">
+              <img class="wpage" src="/pictures/sun" className="d-block w-90" alt="cannot show"/>
+            </div> */}
+          {/* </div>
+        </div> */}
       </div>
   )
 }
